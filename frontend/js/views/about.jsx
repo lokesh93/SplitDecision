1b9ef1da13c4df3b44f204577d18104767a3e440
Lokesh Podipireddy <lpodipireddy@shoplogix.com>
2021-03-30 15:57:25 -0400

    add about page

--- frontend/js/views/about.jsx
+++ frontend/js/views/about.jsx
@@ -3,16 +3,10 @@ import Navigation from '../components/navigation.jsx';
 import axios from 'axios';
 
 
-class CreateGroup extends Component {
+class About extends Component {
 
     constructor(props) {
         super(props);
-        this.state = {
-            date: new Date(),
-            members: [
-                {name: ""}
-            ]
-        }
     }
 
 
@@ -29,6 +23,31 @@ class CreateGroup extends Component {
                             <h3>About</h3>
                         </div>
 
+                        <div className="create-group-form-body">
+                            <p style={{fontWeight: "600"}}>SplitDecision is an application that helps you organize your debts with other people. It is personal project
+                                to learn about django framework.
+                            </p>
+
+                            <ol  style={{textAlign: "left"}}>
+                                <li>
+                                    You simply go to this <a style={{color: "blue"}} href="https://splitdecision.herokuapp.com/app" target="_blank">link</a>.
+                                    Enter the name of your group and the members involved. Once you click submit, it should redirect you to 
+                                    group page.
+                                </li>
+                                <li>
+                                    Once you are on the group page, on the top right corner of the navigation bar is the current user setting.
+                                    Select your name from this menu.
+                                </li>
+                                <li>
+                                    From here you can add your debted items and keep track of who owes whom money.
+                                </li>
+                             </ol>
+
+                             <p><b>Note</b>: This is a work in progress application, features to come are: user management module, Improved 
+                             UI design, mathematical validations and sorting/fitlering</p>
+
+                        </div>
+
                         
                     </div>
                 </div>
@@ -37,4 +56,4 @@ class CreateGroup extends Component {
     }
 }
 
-export default CreateGroup;
+export default About;
